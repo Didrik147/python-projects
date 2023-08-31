cb1a9101632d09d0b4a60dea43d92923e7def3fb
Didrik Roest <didrik.roest@yahoo.com>
2023-08-31 19:04:11 +0200

    blackjack

---
+++ blackjack/README.md
@@ -0,0 +1,6 @@
+# TODO
+- [ ] Variable value for ace (11 or 1)
+- [ ] Add dealer
+- [ ] Add money
+- [ ] Add graphics (pygame?)
+- [ ] Other rules?
--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,24 +1,24 @@
 """
-TODO
-- [ ] Variable value for ace (11 or 1)
-- [ ] Add dealer
-- [ ] Add money
-- [ ] Add graphics (pygame?)
-- [ ] Other rules?
+See "README.md" for information about the progress
 """
 
 import random
 
 suits = ["C", "D", "H", "S"]
+values = list(range(2,11)) + ["J", "Q", "K", "A"]
 
 deck = []
 
+
 # One regular deck of playing cards (no jokers)
 for s in suits:
-    for i in range(1, 13+1):
-        if i > 10:
-            i = 10
-        deck.append(i)
+    for v in values:
+        if v in ["J", "Q", "K"]:
+            deck.append(10)
+        elif v == "A":
+            deck.append(11)
+        else:
+            deck.append(v)
         
 
 # Shuffle the deck
@@ -40,6 +40,8 @@ def drawCard():
 player = []
 
 print("Welcome to blackjack!")
+print("------------------------")
+
 card = drawCard()
 print(f"Your first card is {card}.")
 player.append(card)
@@ -48,11 +50,27 @@ card = drawCard()
 print(f"Your second card is {card}.")
 player.append(card)
 
-total = sum(player)
 
-print(f"Your sum is now {total}.")
+print(f"Your sum is now {sum(player)}.")
+
+print()
+
+# Dealer cards
+dealer = []
+
+# First card (hidden)
+card = drawCard()
+dealer.append(card)
+
+# Second card (public)
+card = drawCard()
+dealer.append(card)
+print(f"Dealers face up card is {card}.")
+
 
 playing = True
+won = True
+bust = False
 
 while playing:
     print()
@@ -63,16 +81,54 @@ while playing:
         card = drawCard()
         print(f"Your next card is {card}.")
         player.append(card)
-        total = sum(player)
-        print(f"Your sum is now {total}.")
+        print(f"Your sum is now {sum(player)}.")
         
     if choice == "s":
         print("You do not receive any more cards.")
         playing = False
         
-    if total > 21:
+    if sum(player) > 21:
         print("You bust!")
         playing = False
+        won = False
+        bust = True
 
 
+if not bust:
+    print()
+    print("Dealers turn")
+    print("------------------")
+
+    print(f"Dealers first card: {dealer[0]}")
+    print(f"Dealers second card: {dealer[1]}")
+    print(f"Dealers total: {sum(dealer)}")
+
+    while sum(dealer) <= 16:
+        print()
+        print("Dealer draws another card")
+        card = drawCard()
+        print(f"Dealer draws a {dealer[0]}")
+        dealer.append(card)
+        print(f"Dealers total: {sum(dealer)}")
+        
+    if sum(dealer) > 21:
+        print("Dealer busts!")
+        dealer = []
+    else:
+        print("Dealer stands.")
+        
+    print()
+    
+    print("You  | Dealer")
+    print("--------------")
+    print(f" {sum(player)}  |  {sum(dealer)}")
+    
+    print()
+    
+    if sum(player) > sum(dealer):
+        print("You win!!!")
+    elif sum(player) < sum(dealer):
+        print("Dealer wins.")
+    else:
+        print("You and the dealer have the same hand-total (push)")
 
