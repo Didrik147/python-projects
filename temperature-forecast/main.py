492da00e8b01a9b66f378352436d31728a0f4a62
Didrik147 <didrik.roest@yahoo.com>
2025-09-16 20:15:52 +0200

    Weather app

---
+++ temperature-forecast/main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+st.title('21 days temperature forecast')
+
+url = 'https://api.met.no/weatherapi/subseasonal/1.0/complete?lat=59.94&lon=10.72'
+
+headers = {
+  'User-Agent': 'Didrik R.',
+  'From': 'didrik.roest@yahoo.com'
+}
+
+response = requests.get(url, headers=headers)
+
+data = response.json()
+
+timeseries = data['properties']['timeseries']
+
+time = []
+air_temperature_mean = []
+
+for t in timeseries:
+  time.append(t['time'])
+  air_temperature_mean.append(t['data']['next_24_hours']['details']['air_temperature_mean'])
+
+
+df = pd.DataFrame(list(zip(time, air_temperature_mean)), columns=['time', 'air_temperature_mean'])
+df['time'] = pd.to_datetime(df['time']).dt.date
+
+df = df.rename(columns={'time':'date'})
+
+st.write(df.head())
